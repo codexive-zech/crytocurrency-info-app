96bfacf13e36b2ac7e6f5fd9f4b3b181c187c899
Codexive_zech <zechariahhounwanou@gmail.com>
2022-09-17 00:16:02 +0100

    Added Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Typography } from "antd";
+import { Link } from "react-router-dom";
+
+const { Title } = Typography;
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Title
+      level={2}
+      className="head"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItem: "center",
+      }}
+    >
+      They Was an Error. kindly go back Home -
+      <Link to="/" className="btn">
+        Click Me
+      </Link>
+    </Title>
+  );
 };
 
 export default Error;
